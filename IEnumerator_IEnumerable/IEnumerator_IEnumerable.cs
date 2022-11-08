760a988342f4eeba68ede71b3bfa8b3a50b2ab86
ccirion01 <cotycirion94@gmail.com>
2022-11-08 08:42:53 -0300

    IEnumerator & IEnumerable

---
+++ IEnumerator_IEnumerable/IEnumerator_IEnumerable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace IEnumerator_IEnumerable
+{
+    public class MyInfiniteEnumerable : IEnumerable<int>
+    {
+        private int[] _myData = new int[] {4, 5, 6};
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new MyInfiniteEnumerator(_myData);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyInfiniteEnumerator(_myData);
+        }
+    }
+
+    public class MyInfiniteEnumerator : IEnumerator<int>
+    {
+        private int[] _data;
+        private int _mIndex = -1;
+
+        public MyInfiniteEnumerator(int[] data)
+        {
+            _data = data;
+        }
+
+        public int Current => _data[_mIndex];
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            _mIndex++;
+            return _mIndex < _data.Length;
+        }
+
+        public void Reset()
+        {
+            _mIndex = 0;
+        }
+    }
+}
---
+++ IEnumerator_IEnumerable/Program.cs
@@ -0,0 +1,77 @@
+﻿using IEnumerator_IEnumerable;
+
+List<string> months = new List<string>();
+months.Add("January");
+months.Add("February");
+months.Add("March");
+months.Add("April");
+months.Add("May");
+months.Add("June");
+months.Add("July");
+months.Add("August");
+months.Add("September");
+months.Add("October");
+months.Add("November");
+months.Add("December");
+
+//create IEnumerable of string
+IEnumerable<string> ienumerable = (IEnumerable<string>)months;
+
+//If we want to retrieve all the items from this IEnumerable object, we can use a foreach loop.
+//foreach (string month in ienumerable)
+//{
+//    Console.WriteLine(month);
+//}
+
+//Create IEnumerator of string by invoking the GetEnumerator() method.
+IEnumerator<string> ienumerator = months.GetEnumerator();
+
+//To retrieve all the items from the above IEnumerator object, we cannot use foreach loop. Instead of that we need to invoke MoveNext() Boolean method.  
+//while (ienumerator.MoveNext())
+//{
+//    Console.WriteLine(ienumerator.Current);
+//}
+
+//Differences
+
+void IEnumeratorMethodOne(IEnumerator<string> i)
+{
+    while (i.MoveNext())
+    {
+        Console.WriteLine(i.Current);
+
+        if (i.Current == "June")
+        {
+            IEnumeratorMethodTwo(i);
+        }
+    }
+}
+
+void IEnumeratorMethodTwo(IEnumerator<string> i)
+{
+    while (i.MoveNext())
+    {
+        Console.WriteLine(i.Current);
+    }
+}
+
+//IEnumeratorMethodOne(ienumerator);
+
+//Example video
+var infinite_enumerable = new MyInfiniteEnumerable();
+foreach (int i in infinite_enumerable)
+{
+    Console.WriteLine($"I is {i}");
+}
+
+var infinite_enumerator = infinite_enumerable.GetEnumerator();
+while (infinite_enumerator.MoveNext())
+{
+    Console.WriteLine($"I is {infinite_enumerator.Current}");
+}
+
+var infinite_enumerable2 = new MyInfiniteEnumerable();
+foreach (int i in infinite_enumerable)
+{
+    Console.WriteLine($"I is {i}");
+}
